cb898b4a3654aae7a185dba9c4e0012219c2303c
ingridliabakk <ier008@uib.no>
2020-09-01 15:29:18 +0200

    open_file()

---
+++ project1/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import csv
+
+def get_X(liste):
+    return liste[:, :4]
+
+def get_lable(liste):
+    return liste[:,4]
+
+
+def open_file(fileName): 
+    return np.loadtxt(open(fileName, "rb"), delimiter=",")
+
+if __name__ == "__main__":
+    liste = open_file('project1/data_banknote_authentication.txt')
+    print('LISTE: ', liste)
+    print('X: ', get_X(liste))
+    print('LABEL: ', get_lable(liste))
+
