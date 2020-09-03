05eb1f1ec0124205f34fcd16087bd21103d37866
Tobias E <tseilertsen@gmail.com>
2020-09-03 16:07:24 +0200

    småting

--- project1/tree.py
+++ project1/tree.py
@@ -31,33 +31,33 @@ class Decision_tree:
     def __init__(self):
         root = Node("")
 
-    def learn(X, y, impurity_measure='entropy'):
+    def learn(self, X, y, impurity_measure='entropy'):
         pass
 
-    def predict(x):
+    def predict(self, x):
         pass
     
-    def less(x, y):
+    def less(self, x, y):
         return x<y
-    def greater(x, y):
+    def greater(self, x, y):
         return x>y
 
     #information gain
-    def IG(x, col, split, X): 
+    def IG(self, x, col, split, X): 
         ig = H(y) - Hcond(x, split, X, less) - (Hcond(x, split, X, greater))
         return ig
     
     #entropy of variable
-    def H(x, split, X, func, col):
+    def H(self, x, split, X, func, col):
         return P(x, X) * np.log2(P(x, X) * P(not x, X))
 
     #conditional entropy given split at column
-    def Hcond(x, split, X, func, col):
+    def Hcond(self, x, split, X, func, col):
         prob = P(X, less, split, col)
         return -prob * np.log2(prob) -(1-prob) * np.log2(1-prob)
 
    # probability that random x is higher/lower 
-    def P(np.array: X, func, split, col): 
+    def P(self, X, func, split, col): 
         column = X[:, col]
         selected = [val for val in column if func(val, split)]
         return len(selected)/len(column)
