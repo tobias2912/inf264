95be1e80dcdcb0e4868b6b7a432d3b8fee79dfc9
Tobias E <tseilertsen@gmail.com>
2020-09-11 16:08:49 +0200

    s

--- project1/main.py
+++ project1/main.py
@@ -20,9 +20,17 @@ if __name__ == "__main__":
     y_train, y_test = y[:1000], y[1000:]
     tree = Decision_tree()
     tree.learn(X_train, y_train, tree.root)
-    new = np.array([-0.10234,1.8189,-2.2169,-0.56725])
-    val = tree.predict(tree.root, new)
-    print(f"value is {val}")
+    wrong = 0
+    correct = 0
+    for rownumber, x in enumerate(X_train):
+        predicted_val = tree.predict(tree.root, x)
+        if predicted_val == y_train[rownumber]:
+            correct += 1
+        else:
+            wrong += 1
+    print(correct, wrong)
+    print(f"correctness {correct/(correct+wrong)}")
+    print("testing data:")
     wrong = 0
     correct = 0
     for rownumber, x in enumerate(X_test):
